cdb56aed8544c773dd4ae1fb94477c8f059b146c
tpreston96 <tpreston96@gmail.com>
2021-03-07 00:25:06 -0600

    connect to Postgres

--- server/keys.js
+++ server/keys.js
@@ -2,7 +2,7 @@ module.exports = {
   redisHost: process.env.redisHost,
   redisPort: process.env.redisPort,
   pgUser: process.env.PGUSER,
-  pghost: process.env.PGHOST,
+  pgHost: process.env.PGHOST,
   pgDatabase: process.env.PGDATABASE,
   pgPassword: process.env.PGPASSWORD,
   pgPort: process.env.PGPORT,
