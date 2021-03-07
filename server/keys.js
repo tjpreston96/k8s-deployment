0389f1d981549e33b8188c6f9ce817868f84b066
tpreston96 <tpreston96@gmail.com>
2021-03-07 00:12:01 -0600

    server keys

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.redisHost,
+  redisPort: process.env.redisPort,
+  pgUser: process.env.PGUSER,
+  pghost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+};
